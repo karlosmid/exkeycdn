9c1fc67442e06f242b1ba90a2aadb79b07ca486a
Karlo Smid <karlo.smid@gmail.com>
2021-02-07 11:14:26 +0100

    package version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExKeyCDN.MixProject do
   def project do
     [
       app: :exkeycdn,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
